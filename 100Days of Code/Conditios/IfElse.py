033d7072fa9dec980557517eedec0fefbd0c3d3d
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-09-18 21:21:44 +0530

    IF else

---
+++ 100Days of Code/Conditios/IfElse.py
@@ -0,0 +1,12 @@
+price=200
+budget=150
+if(budget>price):
+    print("Ok to buy")
+else:
+    print("Not ok")
+num=int(input("Enter num 1-> "))
+num2=int(input("Enter num 2-> "))
+if(num+num2>100):
+    print("Enough Money")
+else:
+    print("No")
