c637a87a410f1db408fca9751eb3fa33515d0d25
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-09-18 21:17:24 +0530

    String Slicing

---
+++ 100Days of Code/String/StringMethods.py
@@ -0,0 +1,23 @@
+#Strings are immutable in java
+a="!!!!Rohan Rohan ROhan!!!!!"
+print(len(a))
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Rohan","Raja"))
+print(a.split(" "))
+blogHeading="intro"
+print(len(blogHeading))
+print(len(blogHeading.center(80)))
+print(a.count("Rohan"))
+print(a.endswith("!"))
+print(a.endswith("an",2,20))
+print(a.find("an"))
+print(a.find("xx"))
+#print(a.index("xx"))
+print(blogHeading.isalnum())
+
+print(a.startswith("!!!!Rohan"))
+print(a.swapcase())
+
