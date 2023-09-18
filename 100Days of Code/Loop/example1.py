28e98ece4014bcda235bfbff331fc3c550bcea1d
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-09-19 00:36:29 +0530

    For Loops

---
+++ 100Days of Code/Loop/example1.py
@@ -0,0 +1,13 @@
+name="Rohan"
+for i in name:
+    print(i)
+    if(i=='a'):
+     print("Hue")
+
+colours= ["Red","Green","Yellow","Blue"]
+print(colours)
+
+for i in colours:
+    print(i)
+    for j in i:
+        print(j)
---
+++ 100Days of Code/Loop/example2.py
@@ -0,0 +1,5 @@
+for k in range(5):
+    print(k)
+print()
+for k in range(1,5):
+    print(k)
