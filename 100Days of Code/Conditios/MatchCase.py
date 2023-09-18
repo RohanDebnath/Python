d1d3c5c6616f33468a66bfb0844919711c771979
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-09-18 23:04:13 +0530

    MatchCase

---
+++ 100Days of Code/Conditios/MatchCase.py
@@ -0,0 +1,29 @@
+a = 4
+match a:
+    case 1:
+      print("January")
+    case 2:
+      print("February")
+    case 3:
+      print("March")
+    case 4:
+      print("April")
+    case 5:
+      print("May")
+    case 6:
+      print("June")
+    case 7:
+      print("July")
+    case 8:
+      print("August")
+    case 9:
+      print("September")
+    case 10:
+      print("Octuber")
+    case 11:
+      print("November")
+    case 12:
+      print("December")
+    case _:
+      print("Please Enter a Valid Month Number")
+
