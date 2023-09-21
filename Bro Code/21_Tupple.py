9afe9a11fdd425cdfc0a938d51aec767fe8c49d2
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-09-21 07:04:08 +0530

    Tupple Done

---
+++ Bro Code/21_Tupple.py
@@ -0,0 +1,6 @@
+# Tuples = () Ordered and unchangeable. Duplicates Ok and faster
+
+fruits=("Apples","Banana","Coconut","Apples")
+print(fruits) 
+print(fruits.index("Apples"))
+print(fruits.count("Apples"))
