c3cecfafa73a5ab10b20656f5ae9d5f5ef9acbbd
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-09-27 12:24:11 +0530

    Keyword Arguments

---
+++ Bro Code/33_Keyword Argument.py
@@ -0,0 +1,5 @@
+def hello(greeting, title,fName, lName ):
+    print(f"{greeting} {title} {fName} {lName}")
+
+hello("Hello",lName="Debnath",fName="Rohan",title="Mr") #positional Argument Follows Keyword Argument
+
