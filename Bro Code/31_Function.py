3f1116e2d1b4b844c48bed7f8bb7ca275b9fcf5b
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-09-27 06:57:26 +0530

    Function Call Done

---
+++ Bro Code/31_Function.py
@@ -0,0 +1,6 @@
+def happy_birthday(name,age):
+    print(f"Happy birthday to {name}")
+    print(f"You are {age} years old")
+happy_birthday("Rohan",21)
+print()
+happy_birthday("Ronaldo",38)
---
+++ Bro Code/31_FunctionCall.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def devide(x,y):
+    return x/y
+
+print(add(6,3))
+print(sub(6,3))
+print(mul(6,3))
+print(devide(6,3))
+
