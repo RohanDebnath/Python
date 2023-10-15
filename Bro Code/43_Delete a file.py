26cfa3289d5f3133acb134ab45d33419473e784f
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-10-15 21:30:42 +0530

    Moving and delete

---
+++ Bro Code/43_Delete a file.py
@@ -0,0 +1,18 @@
+#Does not delete empty folder
+import os
+import shutil
+
+path = "C:\\Users\\USER\\Desktop\\test.txt"
+
+try:
+    os.remove(path)    #delete a file
+    #os.rmdir(path)     #delete an empty directory
+    #shutil.rmtree(path)#delete a directory containing files
+except FileNotFoundError:
+    print("That file was not found")
+except PermissionError:
+    print("You do not have permission to delete that")
+except OSError:
+    print("You cannot delete that using that function")
+else:
+    print(path+" was deleted")
